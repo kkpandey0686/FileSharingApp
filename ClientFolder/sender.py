a1593842e1fc794d2a21fc25e8873330bd169f52
Krishnakant Pandey <kkpandey0686@gmail.com>
2021-05-08 11:55:38 +0530

    first commit

---
+++ ClientFolder/sender.py
@@ -0,0 +1,37 @@
+import socket
+import os
+
+def main():
+    client.send(f"{nickname}".encode('utf-8'))
+    print(client.recv(1024).decode('utf-8'))
+
+    
+    
+
+    while True:
+        filename = input("Enter the Filename: ")
+        if os.path.isfile(filename):
+            client.send(f"{filename}".encode('utf-8'))
+            print(client.recv(1024).decode('utf-8'))
+            
+            with open(filename, "rb") as f:
+                data = f.read()
+                client.sendall(data)
+            break
+
+        else :
+            print(f"File with name {filename} doesn't exist")
+
+    print("File Sent")
+    
+
+if __name__ == '__main__':
+    
+    nickname = input("Enter your nickname: ")
+    
+    client = socket.socket()
+
+    client.connect(('127.0.0.1',9920))
+
+    main()
+    client.close()
---
+++ receiver.py
@@ -0,0 +1,57 @@
+import socket
+import threading
+import time
+
+
+client_data = {}
+def handle_client(nickname, client):
+    client.send("Please Send the Filename".encode('utf-8'))
+    filename = client.recv(1024).decode('utf-8')
+    client.send("Please Send the File".encode('utf-8'))
+
+    start_time = time.time()
+
+    with open(filename, "wb") as f:
+        file_data = client.recv(1048572)
+        count=0
+        while file_data:
+            count+=1
+            print(count)
+            f.write(file_data)
+            file_data = client.recv(1048572)
+
+    print("File Received")    
+    client_data.pop(nickname)
+    end_time = time.time()
+
+    client.send("File Received".encode('utf-8'))
+    
+    print(f"Time: {round(end_time - start_time,3)}")
+
+    client.close()
+
+def main():
+    while True:
+        client, client_address = server.accept()
+        nickname = client.recv(1024).decode('utf-8')
+        client_data[nickname] = {'client': client}
+        print(f"{nickname} is waiting to send a file")
+        
+
+        thread = threading.Thread(target=handle_client, args=(nickname, client,))
+        thread.start()
+        # thread.join()
+        
+
+if __name__=='__main__':
+
+    server = socket.socket()
+    server.bind(('', 9920))
+    server.listen(5)
+    print("Receiver is Ready")
+
+    main()
+    
+
+
+
